14aba862a1bca76e03076f3a8fbdd6ea63241f19
marcoshsbosco <58055884+marcoshsbosco@users.noreply.github.com>
2023-03-02 20:13:08 -0300

    série de taylor manipulada para reduzir multiplicações

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ k = [-1 / fat(3), 1 / fat(5), -1 / fat(7), 1 / fat(9), -1 / fat(11)]
 def taylor(x):
     global k
 
-    seno = x + k[0] * x ** 3 + k[1] * x ** 5 + k[2] * x ** 7 + k[3] * x ** 9 + k[4] * x ** 11
+    seno = x * (1 + x**2 * (k[0] + x**2 * (k[1] + x**2 * (k[2] + x**2 * (k[3] + k[4] * x**2)))))
 
     return seno
 

ede67a23b980269e274320c446efae854685a9d3
marcoshsbosco <58055884+marcoshsbosco@users.noreply.github.com>
2023-03-02 21:52:57 -0300

    implementado aproximante de padé implementado temporização dos cálculos implementado loop rodando os cálculos de -pi/4 a pi-4 de 0.1 em 0.1

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from math import factorial as fat
+from math import pi
+from math import sin
+from time import time
 
 
 # coeficientes k pré-calculados
@@ -8,10 +11,31 @@ k = [-0.16666666666666666, 0.008333333333333333, -0.0001984126984126984, 2.75573
 def taylor(x):
     global k
 
+    t0 = time()
+
     seno = x * (1 + x**2 * (k[0] + x**2 * (k[1] + x**2 * (k[2] + x**2 * (k[3] + k[4] * x**2)))))
+
+    t1 = time()
+
+    print(f"tempo taylor: {t1 - t0} s")
 
     return seno
 
 
-def pade():
-    pass
+def pade(x):
+    t0 = time()
+
+    seno = (0 + 1*x + 0*x**2 + (1/72 - 1/fat(3))*x**3 + (0 + 0)*x**4 + ((1/72) * (-1/fat(3)))*x**5 + ((1/72) * (1/fat(5)) + (-1/fat(7)))*x**7) / (1 + (1/72)*x**2)
+
+    t1 = time()
+
+    print(f"tempo pade: {t1 - t0} ")
+
+    return seno
+
+x = -pi/4
+while x <= pi/4:
+    # print(f"pade = {pade(x)}, taylor = {taylor(x)}, real = {sin(x)}")
+    print(f"sin({x}): taylor errou por {sin(x) - taylor(x)}, pade por {pade(x) - sin(x)}")
+
+    x += 0.1
